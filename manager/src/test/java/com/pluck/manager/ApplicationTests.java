50f7790e766de7927a7552e527208c245fc29cab
GuiHrk <guihenrique17020@gmail.com>
2026-03-17 23:19:31 -0300

    Adicionando configurações base do Spring boot

---
+++ manager/src/test/java/com/pluck/manager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluck.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
