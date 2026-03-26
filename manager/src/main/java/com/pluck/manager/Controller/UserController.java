985032e718343456b5712bdc3159688cc693b11b
GuiHrk <guihenrique17020@gmail.com>
2026-03-25 22:27:45 -0300

    Correção de Service, API com requisição POST está funcional

---
+++ manager/src/main/java/com/pluck/manager/Controller/UserController.java
@@ -0,0 +1,41 @@
+package com.pluck.manager.controller;
+
+import com.pluck.manager.entity.User;
+import com.pluck.manager.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;  
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    //Create
+    @PostMapping
+    public User create(@RequestBody User user){
+        return userService.create(user);
+    }
+    //Read All
+    @GetMapping
+    public List<User> list(){
+        return userService.list();
+    }
+    //Read By Id
+    @GetMapping("/{id}")
+    public User get(@PathVariable Long id){
+        return userService.getById(id);
+    }
+    //Delete
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        userService.delete(id);
+    }
+
+
+}
