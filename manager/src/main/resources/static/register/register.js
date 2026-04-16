a9b490f2cce882edcd381292dfa3c4807af66127
GuiHrk <guihenrique17020@gmail.com>
2026-04-15 22:18:09 -0300

    Integração de frontend a API rest// Consumo da API com javascript

---
+++ manager/src/main/resources/static/register/register.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("cadastroForm");
+
+if(form){
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name")?.value;
+  const email = document.getElementById("email")?.value;
+  const password = document.getElementById("password")?.value;
+
+  console.log(name, email, password);
+
+  try{
+      const response = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      headers:{
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ 
+        name: name,
+        email: email,
+        password: password,
+        role: "USER"
+        })
+      });
+ 
+          alert("Usuário cadastrado com sucesso");
+      }
+     catch (error) {
+      console.error(error);
+      alert("Erro ao cadastrar ");
+    }
+ });
+}
