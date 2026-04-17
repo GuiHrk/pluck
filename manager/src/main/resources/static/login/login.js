56d85beffbc8325ac6e2ec147903c426fcafc6ef
Thiago Souza <thiagosouzasantos1314@gmail.com>
2026-04-16 21:33:19 -0300

    integração do login com API e cadastro funcionando

--- manager/src/main/resources/static/login/login.js
+++ manager/src/main/resources/static/login/login.js
@@ -1,7 +1,39 @@
-document.getElementById("loginForm").addEventListener("submit", function(e){
+const form = document.getElementById("loginForm");
 
-    e.preventDefault()
+if(form){
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-    alert("Login realizado!")
+    const email = document.getElementById("email")?.value;
+    const password = document.getElementById("password")?.value;
 
-})
+    console.log(email, password);
+
+    try {
+      const response = await fetch("http://localhost:8080/users/login", {
+        method: "POST",
+        headers:{
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      });
+
+      if(response.ok){
+        const data = await response.json();
+        alert("Login realizado com sucesso!");
+
+        console.log("Usuário:", data);
+
+      } else {
+        alert("Email ou senha inválidos!");
+      }
+
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao fazer login");
+    }
+  });
+}
