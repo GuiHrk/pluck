17c1b454d9242d3349de659915ccd13378ae3c34
GuiHrk <guihenrique17020@gmail.com>
2026-05-10 16:23:32 -0300

    Correção de rota de login

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -13,7 +13,7 @@ if(form){
     }
 
     try {
-      const response = await fetch("http://localhost:8080/users/login", {
+      const response = await fetch("https://pluck-qebe.onrender.com/users/login", {
         method: "POST",
         headers:{
           "Content-Type": "application/json"
